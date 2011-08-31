436b3f9ed9b61e9c193dd054298ada5e8ffb519e
Dave Paroulek <dparoulek@gmail.com>
2011-08-31 17:32:36 -0400

    turn off paredit

--- my-lisp.el
+++ my-lisp.el
@@ -2,3 +2,15 @@
 
 ;; highlight entire expression
 (setq show-paren-style 'expression)
+
+;; starter kit uses paredit, but I'm not a fan.
+;; turn off paredit 
+(dolist (x '(scheme emacs-lisp lisp clojure))
+  (when window-system
+    (font-lock-add-keywords
+     (intern (concat (symbol-name x) "-mode"))
+     '(("(\\|)" . 'esk-paren-face))))
+  ;; (add-hook
+  ;;  (intern (concat (symbol-name x) "-mode-hook")) 'turn-on-paredit)
+  (add-hook
+   (intern (concat (symbol-name x) "-mode-hook")) 'run-coding-hook))
