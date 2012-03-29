79d1fb3db2fb367eda5d86610363a648257c7c5b
Dave Paroulek <dparoulek@centurion.(none)>
2012-03-29 14:55:54 -0400

    added shortcut C-x C-m for M-x

--- my-misc.el
+++ my-misc.el
@@ -1,3 +1,7 @@
+
+;; M-x meta key shortcut
+(global-set-key (kbd "C-x C-m") 'execute-extended-command)
+
 ;; Markdown Mode
 (setq auto-mode-alist (cons '("\.mkdwn" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\.markdown" . markdown-mode) auto-mode-alist))
