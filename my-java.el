a6011d0a2b1896b4462b5b690d197ea8718a9637
Dave Paroulek <dparoulek@gmail.com>
2012-12-22 13:12:02 -0500

    Lot of updates to java dev environment, details below
    - malabar mode
    - semantic
    - ffip
    - added several snippets to malabar mode
    - use java/cwd-to-package in snippets

--- my-java.el
+++ my-java.el
@@ -1,12 +1,15 @@
 ;; My emacs java development environment
 
-(add-to-list 'load-path (expand-file-name "~/src/emacs/malabar-mode/malabar-1.5-SNAPSHOT/lisp"))
-
+;; Semantic
 (setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
                                   global-semanticdb-minor-mode
                                   global-semantic-idle-summary-mode
-                                  global-semantic-mru-bookmark-mode))
+                                  global-semantic-mru-bookmark-mode
+                                  global-semantic-idle-completions-mode))
 (semantic-mode 1)
+
+;; Malabar Mode
+(add-to-list 'load-path (expand-file-name "~/src/emacs/malabar-mode/malabar-1.5-SNAPSHOT/lisp"))
 (require 'malabar-mode)
 (setq malabar-groovy-lib-dir (expand-file-name "~/src/emacs/malabar-mode/malabar-1.5-SNAPSHOT/lib"))
 (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
@@ -16,20 +19,37 @@
             (add-hook 'after-save-hook 'malabar-compile-file-silently
                       nil t)))
 
-(global-set-key (kbd "C-c m t") '(malabar-run-maven-command "test") )
+;; ffip
+(require 'find-file-in-project)
+(add-to-list 'ffip-patterns "*.xml")
+(add-to-list 'ffip-patterns "*.java")
+(add-to-list 'ffip-patterns "*.json")
+(add-to-list 'ffip-patterns "*.jsp")
+(add-to-list 'ffip-patterns "*.conf")
+(setq ffip-find-options "-not -regex \".*target.*\"")
+(setq ffip-project-file "pom.xml")
+(global-set-key (kbd "C-x f") 'find-file-in-project)
+
 
 ;; Useful elisp java functions
 
 (defun java/cwd-to-package ()
   "Assuming you're using maven directory structure defaults,
-return the package name of the current working directory"
+return the package name of the current working directory. In
+other words, start in cwd, traverse up dir tree until 'java' dir
+is found, then use directory path to create a package name. For
+example, if cwd is
+`/home/user/project/src/test/java/my/project/helpers`, then the
+package name will be `my.project.helpers`"
   (interactive)
-  (let* ((curr (directory-file-name (expand-file-name ".")))
-         (valid-dir (string-match "/src/main/java/" curr)))
+  (let* ((topdir "java")
+         (dirprefix (expand-file-name 
+                     (concat (locate-dominating-file default-directory topdir) topdir "/")))
+         (curr (directory-file-name (expand-file-name ".")))
+         (valid-dir (string-match dirprefix curr)))
     (if valid-dir
         (replace-regexp-in-string
          "/"
          "."
-         (substring curr (match-end 0)))
-      "")))
+         (substring curr (match-end 0))) "")))
 
