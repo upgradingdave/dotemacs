5aaf1240d3a1e314e4af51fb571f1204134a103e
Dave Paroulek <dparoulek@gmail.com>
2013-03-22 17:10:57 -0400

    General cleanup and update to use solarized theme

---
+++ my-scala.el
@@ -0,0 +1,9 @@
+;; ensime
+;;(add-to-list 'load-path "~/code/scala/ensime_2.10.0-RC3-0.9.8.2/elisp")
+;;(require 'ensime)
+
+;; This step causes the ensime-mode to be started whenever
+;; scala-mode is started for a buffer. You may have to customize this step
+;; if you're not using the standard scala mode.
+;;(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
